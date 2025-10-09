04fbdce857a8a9b7ca1da7b8e19aec96e4c2bf82
Juanes Ramirez <juanesramirez99@gmail.com>
2025-10-09 11:08:45 -0500

    fix: update description for enable_versioning variable in S3 bucket module

--- modules/s3-bucket/variables.tf
+++ modules/s3-bucket/variables.tf
@@ -14,7 +14,7 @@ variable "project_name" {
 }
 
 variable "enable_versioning" {
-  description = "Enable versioning for the S3 bucket"
+  description = "Enable versioning for the S3 bucket this is for v3"
   type        = bool
   default     = true
 }
